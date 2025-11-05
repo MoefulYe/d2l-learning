3a9aefb6641c7a1d71a6480be37a93f5f8c6a322
ashenye <luren145@gmail.com>
2025-11-05 20:54:45 +0800

    init

--- flake.nix
+++ flake.nix
@@ -13,42 +13,53 @@
         config.cudaSupport = true;
         config.cudaVersion = "12";
       };
-      # Change according to the driver used: stable, beta
-      nvidiaPackage = pkgs.linuxPackages.nvidiaPackages.stable;
+      python = pkgs.python312;
     in
     {
       # alejandra is a nix formatter with a beautiful output
       formatter."${system}" = nixpkgs.legacyPackages.${system}.alejandra;
-      devShells."${system}".default = (pkgs.buildFHSEnv {
-        name = "vllm-dev-shell";
-        targetPkgs =
-          pkgs:
-          (with pkgs; [
-            cudaPackages.cuda_cudart
-            cudaPackages.nsight_systems
-            cudaPackages.nsight_compute
-            cudaPackages.cudnn
-            llvmPackages_21.clang-tools
-            cudatoolkit
-            python312
-            uv
-            cmake
-            gcc
-	    linuxPackages.nvidia_x11
-            libGLU libGL
-            xorg.libXi xorg.libXmu freeglut
-            xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib 
-            ncurses5 binutils
-            ffmpeg
-          ]);
-	profile = ''
-      	  export CUDA_PATH=${pkgs.cudatoolkit}
-	  export DISABLE_DIRENV=1
-	  export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+      devShells."${system}".default = pkgs.mkShell {
+        name = "d2l";
+        packages = with pkgs; [
+          cudaPackages.cuda_cudart
+          cudaPackages.nsight_systems
+          cudaPackages.nsight_compute
+          cudaPackages.cudnn
+          llvmPackages_21.clang-tools
+          cudatoolkit
+          python
+          uv
+          cmake
+          gcc
+          libgcc.lib
+          glibc
+          linuxPackages.nvidia_x11
+          libGLU
+          libGL
+          xorg.libXi
+          xorg.libXmu
+          freeglut
+          xorg.libXext
+          xorg.libX11
+          xorg.libXv
+          xorg.libXrandr
+          zlib
+          ncurses5
+          binutils
+          ffmpeg
+        ];
+        shellHook = ''
+          export CUDA_PATH=${pkgs.cudatoolkit}
+          export DISABLE_DIRENV=1
+          export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
           export EXTRA_CCFLAGS="-I/usr/include"
-	  export LD_LIBRARY_PATH="${pkgs.linuxPackages.nvidia_x11}/lib"
-	'';
-        runScript = "zsh";
-      }).env;
+          export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.linuxPackages.nvidia_x11}/lib:$(echo "$NIX_LDFLAGS" | grep -o -- '-L[^ ]*' | sed 's/^-L//' | paste -sd:)
+          # 检查是否存在.venv目录, 若不存在则创建一个新的虚拟环境
+          if [ ! -d ".venv" ]; then
+            uv venv
+          fi
+          source .venv/bin/activate
+        '';
+      };
     };
 }
